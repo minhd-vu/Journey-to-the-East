5a87201cef5ba94ff62a8917898b3b38d7f0e36c
Minh Vu <minhd_vu@yahoo.com>
2019-12-26 00:51:47 -0500

    camera follow still jittery

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform target;
+    [SerializeField] private Transform target = null;
+    [SerializeField] private float smoothing = 1f;
 
     void Start()
     {
 
     }
 
-
     void LateUpdate()
     {
-        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        if (transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
     }
 }
