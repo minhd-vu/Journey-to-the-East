94481769d81dc38f697dea0c4c6d8d507713110a
Minh Vu <minhd_vu@yahoo.com>
2019-12-25 23:52:15 -0500

    added audio manager

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -2,25 +2,25 @@
 using UnityEngine;
 
 [System.Serializable]
-public class Sound {
+public class Sound
+{
+    public string name;
 
-	public string name;
+    public AudioClip clip;
 
-	public AudioClip clip;
+    [Range(0f, 1f)]
+    public float volume = .75f;
+    [Range(0f, 1f)]
+    public float volumeVariance = 0.1f;
 
-	[Range(0f, 1f)]
-	public float volume = .75f;
-	[Range(0f, 1f)]
-	public float volumeVariance = 0.1f;
+    [Range(.1f, 3f)]
+    public float pitch = 1f;
+    [Range(0f, 1f)]
+    public float pitchVariance = 0.1f;
 
-	[Range(.1f, 3f)]
-	public float pitch = 1f;
-	[Range(0f, 1f)]
-	public float pitchVariance = 0.1f;
+    public bool loop = false;
 
-	public bool loop = false;
-
-	[HideInInspector]
-	public AudioSource source;
+    [HideInInspector]
+    public AudioSource source;
 
 }
