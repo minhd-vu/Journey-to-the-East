e78d48a34a11f3a1e5d5c31c85756483fe869759
Minh Vu <minhd_vu@yahoo.com>
2019-12-25 23:47:41 -0500

    added ez camera shaker

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound {
+
+	public string name;
+
+	public AudioClip clip;
+
+	[Range(0f, 1f)]
+	public float volume = .75f;
+	[Range(0f, 1f)]
+	public float volumeVariance = 0.1f;
+
+	[Range(.1f, 3f)]
+	public float pitch = 1f;
+	[Range(0f, 1f)]
+	public float pitchVariance = 0.1f;
+
+	public bool loop = false;
+
+	[HideInInspector]
+	public AudioSource source;
+
+}
