973b8d936a8640bbbef36031fb2b165768393b46
Minh Vu <minhd_vu@yahoo.com>
2019-12-27 16:13:05 -0500

    added marketing, fixed bullet animations

---
+++ Assets/DestroyAnimation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     [SerializeField] private GameObject bullet = null;
+    [SerializeField] private GameObject muzzleFlash = null;
     [HideInInspector] public Transform firePoint;
     [SerializeField] private float shakeMagnitude = 1f;
     [SerializeField] private float shakeRoughness = 1f;
     [SerializeField] private float shakeDuration = 0.3f;
+    [SerializeField] private bool isAutomatic = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +21,7 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") )
         {
             Shoot();
         }
@@ -28,6 +30,7 @@ public class Weapon : MonoBehaviour
     void Shoot()
     {
         Instantiate(bullet, firePoint.position, firePoint.rotation);
+        Instantiate(muzzleFlash, firePoint.position, firePoint.rotation);
         CameraShake.instance.ShakeOnce(shakeMagnitude, shakeRoughness, shakeDuration);
     }
 }
