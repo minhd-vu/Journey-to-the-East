ea0c8a09ca8fff520370184d1cb8f1ec80832170
Minh Vu <minhd_vu@yahoo.com>
2019-12-27 16:21:03 -0500

    added automatic fire

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,21 +7,27 @@ public class Weapon : MonoBehaviour
     [SerializeField] private GameObject bullet = null;
     [SerializeField] private GameObject muzzleFlash = null;
     [HideInInspector] public Transform firePoint;
+
     [SerializeField] private float shakeMagnitude = 1f;
     [SerializeField] private float shakeRoughness = 1f;
     [SerializeField] private float shakeDuration = 0.3f;
+
     [SerializeField] private bool isAutomatic = false;
+    private float bulletTimer;
+    [SerializeField] private float bulletsPerSecond;
 
     // Start is called before the first frame update
     void Start()
     {
         firePoint = transform.Find("Fire Point");
+
+        bulletTimer = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1") )
+        if (Input.GetButtonDown("Fire1") || (isAutomatic && Input.GetButton("Fire1") && (bulletTimer += Time.deltaTime) >= 1f / bulletsPerSecond))
         {
             Shoot();
         }
@@ -32,5 +38,6 @@ public class Weapon : MonoBehaviour
         Instantiate(bullet, firePoint.position, firePoint.rotation);
         Instantiate(muzzleFlash, firePoint.position, firePoint.rotation);
         CameraShake.instance.ShakeOnce(shakeMagnitude, shakeRoughness, shakeDuration);
+        bulletTimer = 0f;
     }
 }
