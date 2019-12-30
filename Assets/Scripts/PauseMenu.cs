03465b45ee228fab16626d40a6a8d6897cc70645
Minh Vu <minhd_vu@yahoo.com>
2019-12-30 15:53:41 -0500

    added main menu and pause menu (unfinished

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool paused = false;
+    public GameObject pauseMenu;
+
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+    }
+
+    void Pause()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+    }
+
+    public void Quit()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
+    }
+}
