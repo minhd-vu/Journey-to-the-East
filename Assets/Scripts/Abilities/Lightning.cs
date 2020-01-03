13845cd84a6a8f96d55d5d25aae311d4f8ad5206
Minh Vu <minhd_vu@yahoo.com>
2020-01-03 15:10:39 -0500

    fixed fireball jr not being able to get hit y lighting

--- Assets/Scripts/Abilities/Lightning.cs
+++ Assets/Scripts/Abilities/Lightning.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class Lightning : Ability
 {
     [SerializeField] private GameObject lightning = null;
-    [SerializeField] private GameObject lightningImpact = null;
     [SerializeField] private float radius = 1f;
     [SerializeField] private LayerMask layers = 0;
 
     protected override IEnumerator CastAbility()
     {
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Instantiate(lightning, mousePosition, Quaternion.identity);
-        Destroy(Instantiate(lightningImpact, mousePosition, Quaternion.identity), 5f);
+        Destroy(Instantiate(lightning, mousePosition, Quaternion.identity), 5f);
         AudioManager.instance.Play(sound);
 
         Collider2D[] colliders = Physics2D.OverlapCircleAll(mousePosition, radius, layers);
@@ -22,7 +20,7 @@ public class Lightning : Ability
         {
             Damageable d = collider.GetComponent<Damageable>();
 
-            if (d != null)
+            if (d != null || (d = collider.GetComponentInParent<Damageable>()) != null)
             {
                 float proximity = (mousePosition - (Vector2)d.transform.position).magnitude;
                 float effect = 1 - (proximity / radius);
