f38822dd1ba9f441005c1a8116fc4f367955cb3b
Minh Vu <minhd_vu@yahoo.com>
2020-01-02 22:42:34 -0500

    fixed forest guardian bugs

--- Assets/Scripts/SurpriseEnemy.cs
+++ Assets/Scripts/SurpriseEnemy.cs
@@ -7,6 +7,7 @@ public class SurpriseEnemy : Enemy
 {
     [SerializeField] private float range = 4f;
     private GameObject player;
+    private bool isAwoken;
     // Start is called before the first frame update
     protected override void Start()
     {
@@ -14,17 +15,27 @@ public class SurpriseEnemy : Enemy
         player = GameObject.FindWithTag("Player");
         GetComponent<AIDestinationSetter>().target = player.transform;
         GetComponent<AIPath>().canMove = false;
+        isAwoken = false;
         isAlive = false;
+
+        foreach (Collider2D collider in GetComponentsInChildren<Collider2D>())
+        {
+            collider.enabled = false;
+        }
+
     }
 
     // Update is called once per frame
     protected override void Update()
     {
-        if (!isAlive && Vector2.Distance(player.transform.position , transform.position) <= range)
+        if (!isAwoken && Vector2.Distance(player.transform.position , transform.position) <= range)
         {
             StartCoroutine(Awaken());
         }
-        base.Update();
+        if (isAwoken)
+        {
+            base.Update();
+        }
     }
 
     private IEnumerator Awaken()
@@ -32,6 +43,12 @@ public class SurpriseEnemy : Enemy
         animator.SetTrigger("Awaken");
 
         yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+        isAwoken = true;
+        foreach (Collider2D collider in GetComponentsInChildren<Collider2D>())
+        {
+            collider.enabled = true;
+        }
 
         isAlive = true;
         GetComponent<AIPath>().canMove = true;
