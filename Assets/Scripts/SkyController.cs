c3cb6ad558f508bcee4f9db995aaa8784825b10e
Minh Vu <minhd_vu@yahoo.com>
2020-01-07 20:52:17 -0500

    added sky controller and sky spawner

---
+++ Assets/Scripts/SkyController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyController : MonoBehaviour
+{
+    public float moveSpeed = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y);
+    }
+}
---
+++ Assets/Scripts/SkySpawner.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkySpawner : MonoBehaviour
+{
+    [System.Serializable]
+    private class MinMaxRange
+    {
+        public float min = 0;
+        public float max = 0;
+    }
+
+    [SerializeField] private GameObject[] objects = null;
+    [SerializeField] private MinMaxRange speedRange = null;
+    [SerializeField] private MinMaxRange heightRange = null;
+    [SerializeField] private float time = 0f;
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        while (objects != null)
+        {
+            GameObject b = Instantiate(objects[Random.Range(0, objects.Length)], new Vector2(28, Random.Range(heightRange.min, heightRange.max)), Quaternion.identity);
+            b.GetComponent<SkyController>().moveSpeed = Random.Range(speedRange.min, speedRange.max);
+            yield return new WaitForSeconds(time);
+        }
+    }
+}
