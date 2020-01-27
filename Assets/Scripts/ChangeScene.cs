06c5b134d25cdf1ea22b6af0e901aa6fcddc1e7f
Minh Vu <minhd_vu@yahoo.com>
2020-01-27 10:07:21 -0500

    attempting to enter buildings

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    [SerializeField] private string scene;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            MainMenu.LoadNextScene(GameObject.FindWithTag("Fade to Black").GetComponent<Animator>(), SceneManager.GetSceneByName(scene).buildIndex);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            MainMenu.LoadNextScene(GameObject.FindWithTag("Fade to Black").GetComponent<Animator>(), SceneManager.GetSceneByName(scene).buildIndex);
+        }
+    }
+}
